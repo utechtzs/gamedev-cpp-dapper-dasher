2ef2800ab0ad26f648c59d44d060d7d0377fe6aa
Zechariah Utecht <utechtzs@gmail.com>
2024-06-04 21:27:10 -0500

    jumping with grav, no double jump

--- main.cpp
+++ main.cpp
@@ -1,33 +1,51 @@
 #include "raylib.h"
 
 int main() {
-	const int DELTA_V = 1;
-	const int windowWidth = 800;
-	const int windowHeight = 1000;
+	const int windowWidth = 512;
+	const int windowHeight = 380;
+
+	InitWindow(windowWidth, windowHeight, "Dapper Dasher");
+
+	const int gravity = 1;
+
 	const int rectWidth = 50;
 	const int rectHeight = 80;
 
-	InitWindow(windowWidth, windowHeight, "Dapper Dasher");
-	SetTargetFPS(60);
-
 	int posY = windowHeight - rectHeight;
 	int velocity = 0;
+	bool isInAir = false;
+	int jumpVel = -22;
+
+	SetTargetFPS(60);
 
 	while (!WindowShouldClose()) {
 		BeginDrawing();
 		ClearBackground(WHITE);
 
+		if (posY >= windowHeight - rectHeight) {
+			// rect on the ground
+			velocity = 0;
+			isInAir = false;
+		} else {
+			// rectangle in the air
+			velocity += gravity;
+			isInAir = true;
+		}
+
+		if (IsKeyPressed(KEY_SPACE) && !isInAir) {
+			velocity -= jumpVel;
+		}
+
+		// reset
+		if (IsKeyPressed(KEY_R)) {
+			velocity = 0;
+			posY = windowHeight - rectHeight;
+		}
+		// update position
 		posY += velocity;
 
 		DrawRectangle(windowWidth / 2, posY, rectWidth, rectHeight, GREEN);
 
-		if (IsKeyDown(KEY_SPACE)) {
-			velocity -= DELTA_V;
-			// reset
-		} else if (IsKeyDown(KEY_R)) {
-			velocity = 0;
-			posY = windowHeight - rectHeight;
-		}
 		EndDrawing();
 	}
 	CloseWindow();
